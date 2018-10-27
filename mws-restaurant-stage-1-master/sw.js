5b99bd7198ee52eac0ddec5fa1dcfbb63979a5d2
Onaiwu Solomon <solomononaiwu@gmail.com>
2018-10-27 14:58:14 +0100

    Updated sw.js
    active event added to service worker

--- mws-restaurant-stage-1-master/sw.js
+++ mws-restaurant-stage-1-master/sw.js
@@ -30,9 +30,23 @@ self.addEventListener('install', function(event) {
     );
 });
 
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+
 self.addEventListener('fetch', function(event) {
-
-
     var requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin) {
         if (requestUrl.pathname === '/') {
